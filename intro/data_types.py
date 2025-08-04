1851c3c7134e8f7017f1c0c0481538200ecb84f8
Ignatius-code1 <igidydabbhs@gmail.com>
2025-08-04 22:26:00 +0300

    data

--- intro/data_types.py
+++ intro/data_types.py
@@ -1,15 +1,34 @@
 # snake_case
 
 x=10 #int
+print(x)
+print(type(x))
 y=3.14 #float
+print(type(y))
+print(y)
 pi=3.14 #float
+print(pi)
+print(type(pi))
 is_valid=True #bool
+print(type(is_valid))
+print(is_valid)
 user_name="Alice" #str
+print(user_name)
+print(type(user_name))
 user_age=30 #int
-
+print(user_age)
+print(type(user_age))
 
 #advanced types
 my_array=[1, 2, 3] #list[array]
+print(my_array)
+print(type(my_array))
 my_set={1, 2, 3} #set no duplicate values
+print(my_set)
+print(type(my_set))
 my_array_tuple=(1, 2, 3 ,"apple") #tuple
+print(my_array_tuple)
+print(type(my_array_tuple))
 my_dict={"name": "Alice", "age": 30} #dict key-value pairs
+print(my_dict)
+print(type(my_dict))
