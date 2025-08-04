5ca0759afcfc1821bc9c9587a369df0003d52a5a
Ignatius-code1 <igidydabbhs@gmail.com>
2025-08-04 21:41:43 +0300

    intro

---
+++ intro/data_types.py
@@ -0,0 +1,15 @@
+# snake_case
+
+x=10 #int
+y=3.14 #float
+pi=3.14 #float
+is_valid=True #bool
+user_name="Alice" #str
+user_age=30 #int
+
+
+#advanced types
+my_array=[1, 2, 3] #list[array]
+my_set={1, 2, 3} #set no duplicate values
+my_array_tuple=(1, 2, 3 ,"apple") #tuple
+my_dict={"name": "Alice", "age": 30} #dict key-value pairs
