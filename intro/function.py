6900bc857e9859ca8639ed031eb6a48cbdc09bc7
Ignatius-code1 <igidydabbhs@gmail.com>
2025-08-05 10:22:10 +0300

    function

---
+++ intro/function.py
@@ -0,0 +1,10 @@
+## greets: function <name> (<parameters)
+
+def greet (name,country):
+    print (name)
+    print (country)
+    print("Hi",name,"Your favourite country is",country)
+    print (f"Hi {name}, Your favourite country is", country)
+    return 34
+result1 = greet ("Sam","USA") 
+result2 = greet ("Alice","Canada")
