d09b67c40c8054dc972fa13827d405d501c997ec
Ignatius-code1 <igidydabbhs@gmail.com>
2025-08-07 15:39:17 +0300

    intro

---
+++ classes/intro.py
@@ -0,0 +1,18 @@
+class Student:
+    name = "Sam"
+    age = 21
+    email = "XXXXXXXXXXXXX" 
+    phone= "254745964638"
+
+student1 = Student()
+Student2 = Student()
+
+print("student 1")
+print( "name",student1.name)
+print("email",student1.email)
+print( "phone", student1.phone)
+
+print("student 2")
+print( "name",Student2.name)
+print("email",Student2.email)
+print( "phone", Student2.phone)
